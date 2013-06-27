4ecaa05c2cf774c00a4805be0a70de24c65e02b5
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-06-27 17:34:04 +0100

    First commit. Bringing the joy of Jade to the clojure community

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-jade "0.1.0-SNAPSHOT"
+  :description "Thin wrapper around jade4j to promote wider use within the clojure community"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [de.neuland/jade4j "0.3.12"]]
+  :repositories [["jade4j-releases" "https://raw.github.com/neuland/jade4j/master/releases"]])
---
+++ src/clj_jade/core.clj
@@ -0,0 +1,3 @@
+(ns clj-jade.core)
+
+
