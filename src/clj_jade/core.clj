a4194d0da2cc78166154109842048f1af12c998f
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-06-27 18:26:25 +0100

    Thin wrapper around rendering using a default jade config

--- src/clj_jade/core.clj
+++ src/clj_jade/core.clj
@@ -1,3 +1,12 @@
-(ns clj-jade.core)
+(ns clj-jade.core
+  (:import [de.neuland.jade4j JadeConfiguration]))
 
+(def config (JadeConfiguration.))
 
+(defn- template
+  [template-path]
+  (.getTemplate config template-path))
+
+(defn render
+  [template-path data]
+  (.renderTemplate config (template template-path) data))
