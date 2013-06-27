0ad69dd0c0f0eeb9c61026ce71d496dca41063b9
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-06-27 18:53:08 +0100

    Storing the default JadeConfig instance in an atom for later change if required

--- src/clj_jade/core.clj
+++ src/clj_jade/core.clj
@@ -1,12 +1,23 @@
 (ns clj-jade.core
   (:import [de.neuland.jade4j JadeConfiguration]))
 
-(def config (JadeConfiguration.))
+(defonce _config (atom (JadeConfiguration.)))
+
+(defn default-config
+  ""
+  [config]
+  (reset! _config config))
+
+(defn configure
+  "Configures the underlying JadeConfig with the options passed. The config created upon
+   the most recent invocation will be used when rendering templates"
+  [opts]
+  (default-config config))
 
 (defn- template
   [template-path]
-  (.getTemplate config template-path))
+  (.getTemplate @_config template-path))
 
 (defn render
   [template-path data]
-  (.renderTemplate config (template template-path) data))
+  (.renderTemplate @_config (template template-path) data))
