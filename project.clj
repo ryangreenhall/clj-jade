9da4001d5d4e938bd2ea6548b6bbb5d25468244f
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-07-03 10:53:36 +0100

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jade "0.1.2"
+(defproject clj-jade "0.1.3"
   :description "Thin wrapper around jade4j"
   :url "https://github.com/ryangreenhall/clj-jade"
   :license {:name "Eclipse Public License"
