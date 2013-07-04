14df591898c4fe752c4475d73657b919b5463933
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-07-04 13:25:38 +0100

    Releasing support for helpers

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jade "0.1.3"
+(defproject clj-jade "0.1.4"
   :description "Thin wrapper around jade4j"
   :url "https://github.com/ryangreenhall/clj-jade"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,4 @@
   :dependencies [[de.neuland/jade4j "0.3.12"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :repositories [["jade4j-releases" "https://raw.github.com/neuland/jade4j/master/releases"]]
-  :aot          :all
-  )
+  :aot          :all)
