a9c637e6aff646dda88bdda5e6a23cf0661e2b54
Ryan Greenhall <ryan.greenhall@gmail.com>
2014-05-20 12:50:33 +0100

    Bumping the version to release Luca's no {} needed to render change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jade "0.1.4"
+(defproject clj-jade "0.1.5"
   :description "Thin wrapper around jade4j"
   :url "https://github.com/ryangreenhall/clj-jade"
   :license {:name "Eclipse Public License"
