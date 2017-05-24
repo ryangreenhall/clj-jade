d6e80210001b4d49e54c469d2af7ee374a70a508
masztal <michal.masztalski@gmail.com>
2017-05-24 16:33:50 +0200

    deps update

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-jade "0.1.7"
+(defproject clj-jade "0.1.8"
   :description "Thin wrapper around jade4j"
   :url "https://github.com/ryangreenhall/clj-jade"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[de.neuland-bfi/jade4j "0.4.2"] [org.clojure/clojure "1.6.0"]]
+  :dependencies [[de.neuland-bfi/jade4j "1.2.1"] [org.clojure/clojure "1.8.0"]]
   :repositories [["jade4j-releases" "https://raw.github.com/neuland/jade4j/master/releases"]]
   :aot          :all)
